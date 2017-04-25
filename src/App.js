16fd38ec2cef76da7e5207df9a0debe6b3dc1727
cduzan <cduzan@themarinergroup.net>
2017-04-25 17:25:13 -0400

    encapsulate entire page in MuiThemeProvider(#1)

--- src/App.js
+++ src/App.js
@@ -1,71 +1,69 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import TextField from 'material-ui/TextField';
 import logo from './logo.svg';
 import './App.css';
 
+injectTapEventPlugin();
+
 const Home = () => (
     <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-      <p className="App-intro">
-        Hello World!
-      </p>
-      <div>
-        <Link to="/new-recipe">Add Recipe</Link>
-      </div>
+        <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+
+            <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        <p className="App-intro">
+            Hello World!
+        </p>
+
+        <div>
+            <Link to="/new-recipe">Add Recipe</Link>
+        </div>
     </div>
 )
 
 const AddRecipe = () => (
     <div>
-      <h2>New Recipe</h2>
-        <MuiThemeProvider>
-            <TextField hintText="Enter name here" floatingLabelText="Name"/>
-        </MuiThemeProvider>
-        <MuiThemeProvider>
-            <TextField hintText="Enter URL here" floatingLabelText="URL"/>
-        </MuiThemeProvider>
+        <h2>New Recipe</h2>
+        <TextField floatingLabelText="Name"/>
+        <TextField floatingLabelText="URL"/>
         <br/>
-        <MuiThemeProvider>
-            <TextField hintText="" floatingLabelText="Servings"/>
-        </MuiThemeProvider>
-        <MuiThemeProvider>
-            <TextField hintText="" floatingLabelText="Difficulty"/>
-        </MuiThemeProvider>
-        <MuiThemeProvider>
-            <TextField hintText="" floatingLabelText="Active Time"/>
-        </MuiThemeProvider>
-        <MuiThemeProvider>
-            <TextField hintText="" floatingLabelText="Total Time"/>
-        </MuiThemeProvider>
+        <TextField floatingLabelText="Servings"/>
+        <TextField floatingLabelText="Difficulty"/>
+        <TextField floatingLabelText="Active Time"/>
+        <TextField floatingLabelText="Total Time"/>
         <br/>
+
         <h3>Ingredients</h3>
-        <MuiThemeProvider>
-            <TextField hintText="" floatingLabelText="Sections"/>
-        </MuiThemeProvider>
+        <TextField floatingLabelText="Sections"/>
         <br/>
     </div>
 )
 
 class App extends Component {
-  render() {
-    return (
-        <Router>
-          <div>
+    render() {
+        return (
+            <Router>
 
-            <Route exact path="/" component={Home}/>
-            <Route path="/new-recipe" component={AddRecipe}/>
-          </div>
-        </Router>
-    );
-  }
+                <MuiThemeProvider>
+                    <div>
+
+                        <Route exact path="/" component={Home}/>
+
+                        <Route path="/new-recipe" component={AddRecipe}/>
+
+                    </div>
+                </MuiThemeProvider>
+            </Router>
+        );
+    }
 }
 
 export default App;
